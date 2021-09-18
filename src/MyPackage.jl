520f48bb7866883204665f71d45c5019d84397bc
Alexandre Bergel <alexandre.bergel@me.com>
2021-09-17 22:25:19 -0300

    Started to work on a hierarchy of shapes

--- src/MyPackage.jl
+++ src/MyPackage.jl
@@ -1,5 +1,49 @@
 module MyPackage
 
+export Shape, getPosition, setPosition!
+export Box
+export Circle, getSize, setSize!
+export Composite, numberOfShapes, addShape!
+
 greet() = print("Hello World!")
 
+abstract type Shape end
+function getPosition(s::Shape)
+    return s.position
+end
+
+function setPosition!(s::Shape, newPosition::Tuple{Float64, Float64})
+    return s.position = newPosition
+end
+
+mutable struct Box <: Shape
+    position::Tuple{Float64, Float64}
+    eventCallbacks::Vector
+    height::Float64
+    width::Float64
+end
+Box() = Box((0.0, 0.0), [], 5, 5)
+
+mutable struct Circle <: Shape
+    position::Tuple{Float64, Float64}
+    eventCallbacks::Vector
+    size::Float64
+end
+Circle() = Circle((0.0, 0.0), [], 5)
+getSize(c::Circle) = c.size
+setSize!(c::Circle, aSize::Int64) = setSize!(c::Circle, Float64(aSize))
+function setSize!(c::Circle, aSize::Float64)
+    c.size = aSize
+end
+
+mutable struct Composite <: Shape
+    position::Tuple{Float64, Float64}
+    eventCallbacks::Vector
+    shapes::Vector{Shape}
+end
+Composite() = Composite((0.0, 0.0), [], [])
+function addShape!(c::Composite, childShape::Shape)
+    push!(c.shapes, childShape)
+end
+numberOfShapes(c::Composite) = length(c.shapes)
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,28 @@
 using Test, MyPackage
 
 @test 42 == (40 + 2)
+
+
+aShape = Box()
+@test aShape.position == (0.0, 0.0)
+@test getPosition(aShape) == (0.0, 0.0)
+@test getPosition(aShape) == (0, 0)
+
+setPosition!(aShape, (2.0, 3.0))
+@test getPosition(aShape) == (2.0, 3.0)
+@test getPosition(aShape) == (2, 3)
+
+
+aCircle = Circle()
+@test getSize(aCircle) == 5
+setSize!(aCircle, 15.0)
+@test getSize(aCircle) == 15
+setSize!(aCircle, 24)
+@test getSize(aCircle) == 24
+
+composite = Composite()
+@test numberOfShapes(composite) == 0
+addShape!(composite, aCircle)
+@test numberOfShapes(composite) == 1
+addShape!(composite, aShape)
+@test numberOfShapes(composite) == 2
